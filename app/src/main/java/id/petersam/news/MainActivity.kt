ea28dba161cab4318010d102dc333b149c2c41ea
Sampurno Aji <man.sanji23@gmail.com>
2020-06-21 10:32:14 +0700

    setup navigation

--- app/src/main/java/id/petersam/news/MainActivity.kt
+++ app/src/main/java/id/petersam/news/MainActivity.kt
@@ -1,12 +1,24 @@
 package id.petersam.news
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
